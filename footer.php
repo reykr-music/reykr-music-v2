04de8f95846d91e7bb54c9c301991fe9063d85b6
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2021-01-13 13:30:51 -0500

    updated footer and 404 page

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 <footer>
   <div class="wrapper">
-    <p><a href="https://lucassilbernagel.com/" target="_blank" rel="noopener noreferrer">Lucas Silbernagel</a> <i class="far fa-copyright" aria-label="copyright"></i> 2020</p>
+    <p>Built by <a href="https://lucassilbernagel.com/" target="_blank" rel="noopener noreferrer">Lucas Silbernagel</a></p>
   </div>
 
   <!-- Button to scroll back to top of page -->
